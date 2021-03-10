fa816fa54fd9cb16a71fcace0a44eea55d3c2fe3
XcenaX <vlad-057@mail.ru>
2021-03-10 14:23:36 +0600

    redis

--- adminpanel/routing.py
+++ adminpanel/routing.py
@@ -1,22 +1,13 @@
-# from django.urls import path
-# from channels.routing import ProtocolTypeRouter, URLRouter
-# from . import consumers
-
-# application = ProtocolTypeRouter({
-#     "websocket": URLRouter([
-#         path("ws/timers/", consumers.TimersConsumer.as_asgi()),
-#     ]),
-# })
-
-
-# chat/routing.py
-from django.urls import re_path
-
+from django.urls import path
+from channels.routing import ProtocolTypeRouter, URLRouter
 from . import consumers
 
-websocket_urlpatterns = [
-    re_path("ws/timers/", consumers.TimersConsumer.as_asgi()),
-]
+application = ProtocolTypeRouter({
+    "websocket": URLRouter([
+        path("ws/timers/", consumers.TimersConsumer.as_asgi()),
+    ]),
+})
+
 
 # from channels.routing import route
 # from .consumers import websocket_receive
--- qfit/settings.py
+++ qfit/settings.py
@@ -116,7 +116,6 @@ CHANNEL_LAYERS = {
         "CONFIG": {
             "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
         },
-        "ROUTING": "adminpanel.routing.websocket_urlpatterns",
     },
 }
 
