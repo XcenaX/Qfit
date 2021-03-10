4b8145ebf1c860d000a52b6505de15c6e3723aa7
XcenaX <vlad-057@mail.ru>
2021-03-10 12:08:49 +0600

    test

--- qfit/settings.py
+++ qfit/settings.py
@@ -182,7 +182,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-
+#
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = "digital.portfolio.ru@gmail.com"
 EMAIL_HOST_PASSWORD = 'Dagad582#'
