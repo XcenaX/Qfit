3a394545e675312911c5b712165d91e30a913f96
XcenaX <vlad-057@mail.ru>
2021-03-10 13:41:35 +0600

    redis

--- qfit/settings.py
+++ qfit/settings.py
@@ -116,7 +116,7 @@ CHANNEL_LAYERS = {
         "CONFIG": {
             "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
         },
-        "ROUTING": "chat.routing.channel_routing",
+        "ROUTING": "adminpanel.routing.application",
     },
 }
 
