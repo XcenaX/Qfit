b3b601e4e0900b865b05410e58b7088b3a54d230
XcenaX <vlad-057@mail.ru>
2021-03-11 21:05:11 +0600

    redis

--- qfit/asgi.py
+++ qfit/asgi.py
@@ -8,21 +8,14 @@
 
 
 
-# import os
-# import django
-# from channels.routing import get_default_application
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qfit.settings')
-# django.setup()
-# application = get_default_application()
-
-
-
 import os
-from channels.asgi import get_channel_layer
+import django
+from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qfit.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qfit.settings')
+django.setup()
+application = get_default_application()
 
-channel_layer = get_channel_layer()
+
 
 
