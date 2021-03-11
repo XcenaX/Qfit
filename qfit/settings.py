9d37e59443f681900dadcce4c1beab19d60a924b
XcenaX <vlad-057@mail.ru>
2021-03-11 20:46:39 +0600

    redis

--- qfit/settings.py
+++ qfit/settings.py
@@ -114,8 +114,9 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+            "hosts": [os.environ.get('REDIS_URL')],
         },
+        'ROUTING': 'adminpanel.routing.application',
     },
 }
 
