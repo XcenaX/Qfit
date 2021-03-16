c6d2004b742d403497d56921b6825b5294e03b8b
XcenaX <vlad-057@mail.ru>
2021-03-16 17:25:25 +0600

    token

--- test_api.py
+++ test_api.py
@@ -1,9 +1,10 @@
 import requests
 import json
 
-data = requests.post("http://127.0.0.1:8000/api/token/", data={"username": "test", "password": "12345"})
+data = requests.post("https://qrfit.herokuapp.com/api/token/", data={"username": "test", "password": "12345"})
+print(data.text)
 token = json.loads(data.text)["access"]
 token_parameter = "Bearer " + token
 print(token_parameter)
-data = requests.get("http://127.0.0.1:8000/api/services/", headers={"Authorization": token_parameter})
+data = requests.get("https://qrfit.herokuapp.com/api/services/", headers={"Authorization": token_parameter})
 print(data.text)
