90ffd26cf7508eba01847b65eea1d0b27472ac01
XcenaX <vlad-057@mail.ru>
2021-03-16 17:52:33 +0600

    token added

--- test_api.py
+++ test_api.py
@@ -1,9 +1,9 @@
 import requests
 import json
 
-data = requests.post("http://127.0.0.1:8000/api/token/", data={"username": "test", "password": "12345"})
+data = requests.post("https://qrfit.herokuapp.com/api/token/", data={"username": "test", "password": "12345"})
 token = json.loads(data.text)["token"]
 token_parameter = "Token " + token
 print(token_parameter)
-data = requests.get("http://127.0.0.1:8000/api/services/", headers={"Authorization": token_parameter})
+data = requests.get("https://qrfit.herokuapp.com/api/services/", headers={"Authorization": token_parameter})
 print(data.text)
